38d49898f28e8c004466e8f0fc8df6e0aaa8f27b
jetpackpony <jetpackpony@gmail.com>
2020-11-03 15:38:15 +0300

    Add resetting total counter at the beginning of a new day

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,29 @@
-import { getData, setData, setStartedAt } from './storage.js';
+import { getData, setData, setStartedAt, resetTotal } from './storage.js';
+
+const msInADay = 24 * 60 * 60 * 1000;
+// Set the date cutoff to 5 am
+const cutOffHour = 5;
+const getCurrentDayStart = () => {
+  const date = new Date();
+  if (date.getHours() < 5) {
+    date.setDate(date.getDate() - 1);
+  }
+  date.setHours(cutOffHour, 0, 0, 0);
+  return date.getTime();
+};
 
 const startTimer = async () => {
-  let { total, startedAt } = await getData();
+  let { total, startedAt, dayStart } = await getData();
+  // If there is no timer running
   if (!Number.isInteger(startedAt)) {
     console.log("Starting timer");
+    console.log(`dayStart: ${dayStart}`);
+
+    // if dayStart not set || it is a new day, reset total
+    if (!Number.isInteger(dayStart) || (Date.now() - dayStart) >= msInADay) {
+      await resetTotal(getCurrentDayStart());
+      total = 0;
+    }
     startedAt = await setStartedAt(Date.now());
   }
   return { total, startedAt };
--- src/js/storage.js
+++ src/js/storage.js
@@ -1,6 +1,6 @@
-export const getData = async () => {
+export const getData = async (fields = { total: 0, startedAt: null, dayStart: null }) => {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get({ total: 0, startedAt: null }, (data) => {
+    chrome.storage.local.get(fields, (data) => {
       resolve(data);
     });
   });
@@ -18,6 +18,20 @@ export const setData = async (data) => {
   return new Promise((resolve, reject) => {
     chrome.storage.local.set(data, () => {
       resolve(data);
+    });
+  });
+};
+
+export const resetTotal = async (newDayStart) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get({ total: 0, dayStart: null, archive: {} }, (data) => {
+      if (data.dayStart) {
+        data.archive[data.dayStart] = data.total;
+      }
+
+      chrome.storage.local.set({ archive: data.archive, total: 0, dayStart: newDayStart }, () => {
+        resolve({ total: 0, dayStart: newDayStart });
+      });
     });
   });
 };
